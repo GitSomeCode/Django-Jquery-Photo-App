d66e98fc193338b5caeb9df01ad8b0d61cc15e6c
GitSomeCode <sjbitcode@gmail.com>
2014-11-02 19:20:28 -0500

    optimistic cache

--- UploadPic2/settings.py
+++ UploadPic2/settings.py
@@ -26,11 +26,13 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 IMAGEKIT_CACHEFILE_DIR = 'sangeeta/'
 AWS_ACCESS_KEY_ID = '...'
 AWS_SECRET_ACCESS_KEY = '...'
 AWS_STORAGE_BUCKET_NAME = 'minidjangoproject'
+IMAGEKIT_DEFAULT_CACHEFILE_STRATEGY = 'imagekit.cachefiles.strategies.Optimistic'
 
 
 # Application definition
